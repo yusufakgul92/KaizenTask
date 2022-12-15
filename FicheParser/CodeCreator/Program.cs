ba610b55efbce5ff9d5716f3069d108d31a1c29e
yakgul <yusufa@ebilgi.com.tr>
2022-12-15 09:05:55 +0300

    recheck

--- FicheParser/CodeCreator/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using CodeCreator;
-Console.WriteLine("Hello, World!");
-
-CodeGenerator codeGenerator = new CodeGenerator();
-codeGenerator.FillHash();
-Console.ReadLine();
